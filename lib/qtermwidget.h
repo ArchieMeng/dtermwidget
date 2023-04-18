5d863d8df8404aeae98af5d382fb3837199b0d8f
Yutao Meng <mengyutao@uniontech.com>
2023-04-18 17:32:26 +0800

    feat: 终端设置增加在双击选中区域包含特殊符号选项
    Description: 本次提交提供在终端设置选项里面，自定义双击选中区域包含特殊符号的功能。
    
    Log: 终端设置增加在双击选中区域包含特殊符号选项
    
    Author: liwl1991 <yunweinote@126.com>
    Signed-off-by: Yutao Meng <mengyutao@uniontech.com>

--- lib/qtermwidget.h
+++ lib/qtermwidget.h
@@ -114,6 +114,8 @@ public:
     //Text codec, default is UTF-8
     void setTextCodec(QTextCodec * codec) override;
 
+    void setTerminalWordCharacters(const QString &wc);
+
     /** @brief Sets the color scheme, default is white on black
      *
      * @param[in] name The name of the color scheme, either returned from
